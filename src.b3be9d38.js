961e204186eed7471b7491ec067e26051ea15547
Serhii-Sunko <86072871+Serhii-Sunko@users.noreply.github.com>
2021-12-25 23:09:17 +0000

    Deploying to gh-pages from @ Serhii-Sunko/lacosta-markup@6a80f6d509ddae15bea4b3d5663a9e3ce7a2827e 🚀

--- src.b3be9d38.js
+++ src.b3be9d38.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/lacosta-markup/src.24595f0f.js.map
+//# sourceMappingURL=/lacosta-markup/src.b3be9d38.js.map
