6a80f6d509ddae15bea4b3d5663a9e3ce7a2827e
Serhii-Sunko <sergeysunko01@gmail.com>
2021-12-26 01:07:15 +0200

    make mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,19 +4,34 @@
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  // refs.backdrop.addEventListener('click', backdropClick);
+
+  // function toggleModal() {
+  //   refs.modal.classList.toggle('is-hidden');
+  //   window.addEventListener('keydown', handle);
+  // }
+
+  // function handle(e) {
+  //   if (e.code === 'Escape') {
+  //     refs.modal.classList.toggle('is-hidden');
+  //     window.removeEventListener('keydown', handle);
+  //   }
+  // }
+
+  function backdropClick(e) {
+    if (e.target !== e.currentTarget) return;
+    refs.modal.classList.toggle('is-hidden');
+  }
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
