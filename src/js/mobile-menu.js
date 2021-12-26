298ee6096b04f697b698bfcbe2a335ba3b7a4c1f
Serhii-Sunko <sergeysunko01@gmail.com>
2021-12-26 15:13:55 +0200

    make markup for main

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const navLinks = document.querySelectorAll('[data-section-link]');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -14,24 +15,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  // refs.backdrop.addEventListener('click', backdropClick);
-
-  // function toggleModal() {
-  //   refs.modal.classList.toggle('is-hidden');
-  //   window.addEventListener('keydown', handle);
-  // }
-
-  // function handle(e) {
-  //   if (e.code === 'Escape') {
-  //     refs.modal.classList.toggle('is-hidden');
-  //     window.removeEventListener('keydown', handle);
-  //   }
-  // }
-
-  function backdropClick(e) {
-    if (e.target !== e.currentTarget) return;
-    refs.modal.classList.toggle('is-hidden');
-  }
+  navLinks.forEach(link => link.addEventListener('click', toggleMenu));
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
